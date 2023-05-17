4419c7557214108a2dea166f4bc0e272fccab29a
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-05-16 23:34:59 -0400

    tables

--- casino-app/app.js
+++ casino-app/app.js
@@ -1,29 +1,29 @@
-import React, { useEffect } from 'react';
-import express from "express";
-import prisma from "./lib/prisma";
+// import React, { useEffect } from 'react';
+// import express from "express";
+// import prisma from "./lib/prisma";
 
-const App = () => {
-    const app = express();
+// const App = () => {
+//     const app = express();
     
-    useEffect(() => {
-        app.use(express.json());
+//     useEffect(() => {
+//         app.use(express.json());
 
-        const PORT = process.env.PORT || 5555;
+//         const PORT = process.env.PORT || 5555;
 
-        app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-    }, []);
+//         app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+//     }, []);
     
-    app.get("/users", async (req, res) => {
-        try {
-        const users = await prisma.user.findMany()
+//     app.get("/users", async (req, res) => {
+//         try {
+//         const users = await prisma.user.findMany()
     
-        res.json(users)
-        } catch (error) {
-        res.status(500).json({
-            message: "Something went wrong",
-        })
-        }
-    })
-};
+//         res.json(users)
+//         } catch (error) {
+//         res.status(500).json({
+//             message: "Something went wrong",
+//         })
+//         }
+//     })
+// };
 
-export default App;
+// export default App;
