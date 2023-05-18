e9514b2a98adde10cf0b887decb697979c0d24ce
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-05-18 14:51:02 -0400

    env changes

--- casino-app/app/layout.js
+++ casino-app/app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <UserProvider>
+        <body className={inter.className}>
+          {children}
+          </body>
+      </UserProvider>
     </html>
   )
 }
--- casino-app/app/myprofile/page.js
+++ casino-app/app/myprofile/page.js
@@ -1,13 +1,25 @@
-import prisma from "../../lib/prisma";
-import Link from "next/link";
+"use client";
+
 import Navbar from "@/components/Navbar";
 import '../styles/profile.css';
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default async function MyProfile () {
+    const { user, error, isLoading } = useUser();
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>{error.message}</div>;
+
     return (
-        <div>
+        user && (
+        <div className="profile-body">
             <Navbar/>
-            <div className="profile-body">my profile</div>
+            <div className="profile">
+                <img src={user.picture} alt={user.name} />
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+            </div>
         </div>
     )
+    );
 }
