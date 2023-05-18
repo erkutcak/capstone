fe15636dbdc10b447eb40eb4d2732b88f30e8fb0
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-05-18 16:32:32 -0400

    auth0

--- casino-app/app/games/page.js
+++ casino-app/app/games/page.js
@@ -3,12 +3,30 @@ import Link from "next/link";
 import Navbar from "@/components/Navbar";
 import '../styles/games.css';
 
+async function getGames() {
+    const games = await prisma.game.findMany()
+    return games
+}
+
 export default async function Games () {
+    const data = await getGames()
+
+    const showGames = data.map((game) => {
+        return (
+            <li>
+                {game.name}
+            </li>
+        )
+    })
 
     return (
-        <div>
+        <div className="games-body">
             <Navbar/>
-            <div className="games-body">games</div>
+            <div className="games-cards">
+                <ul>
+                    {showGames}
+                </ul>
+            </div>
         </div>
     )
 }
